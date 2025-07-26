00501d059b4afe0be28f95cb601ed1e4b08cc143
Jorge Francisco <uniquetekbra@gmail.com>
2025-07-26 12:44:27 -0300

    Commit inicial

---
+++ src/main/java/com/example/cloudconfigpropserver/CloudConfigPropServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.cloudconfigpropserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigPropServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigPropServerApplication.class, args);
+	}
+}
